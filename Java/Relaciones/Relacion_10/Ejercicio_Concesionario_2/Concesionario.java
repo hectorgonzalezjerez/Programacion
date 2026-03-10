ff1ad92040908c8bf7f09941025bd64007fa8397
hectoor15 <hgonjer1301@g.educaand.es>
2026-03-10 11:05:28 +0100

    Clase Coche y Venta terminadas (No Main) y errores en copmrarAFabrica y registrarVenta solucionados

--- Java/Relaciones/Relacion_10/Ejercicio_Concesionario_2/Concesionario.java
+++ Java/Relaciones/Relacion_10/Ejercicio_Concesionario_2/Concesionario.java
@@ -1,20 +1,44 @@
 package Ejercicio_Concesionario_2;
+import java.util.Scanner;
 
 public class Concesionario {
-    private Coche[] catalogo;
+    private Coche[] stock;
     private Venta[] historialVentas;
-    private int numCoches;
-    private int numVentas;
+    private int numCoches = 0;
+    private int numVentas = 0;
 
-    public Concesionario(int capacidadCatalogo, int capacidadVentas){
+    Scanner scanner = new Scanner(System.in);
 
+    //Cnstr
+    public Concesionario(int capacidadStock, int capacidadVentas){
+        this.stock = new Coche[capacidadStock];
+        this.historialVentas = new Venta[capacidadVentas];
     }
 
     
-    //
-
-
-    public RegistrarVenta(String Coche.getMatricula(), String cliente, String fecha, double precio){
-
+    //ComprarAFabrica
+    public boolean comprarAFabrica(String matricula, String marca, String modelo, double precioBase){
+        
+        if( numCoches < stock.length){
+            Coche nuevoCoche = new Coche(matricula, marca, modelo, precioBase);
+            this.stock[numCoches] = nuevoCoche;
+            numCoches ++;
+            return true;
+        }
+        return false;
     }
+
+    //ResgistrarVenta
+    public boolean resgistrarVenta(int idVenta, Coche cocheVendido, String nombreCliente, String fechaVenta, double precioFinal){
+        
+        if( numVentas < historialVentas.length){
+            Venta nuevaVenta = new Venta(idVenta, cocheVendido, nombreCliente, fechaVenta, precioFinal);
+            this.historialVentas[numVentas] = nuevaVenta;
+            numVentas ++;
+            return true;
+        }
+        return false;
+    }
+
+    
 }
